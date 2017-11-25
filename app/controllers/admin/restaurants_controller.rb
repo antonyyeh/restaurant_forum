be1520c35ccf6c15abfa8afd17b3f5c8ac49c0aa
antonyyeh <foxflyy@gmail.com>
2017-11-25 16:32:06 +0800

    add restaurant edit and update actions and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,15 +1,14 @@
 class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+  before_action :set_restaurant, only: [:show, :edit, :update]
   def index
     @restaurants = Restaurant.all
   end
   def new
     @restaurant = Restaurant.new
   end
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
+  
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
@@ -20,9 +19,20 @@ class Admin::RestaurantsController < ApplicationController
       flash[:alert] = "restaurant was failed to create"
     end 
   end
-
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to admin_restaurant_path(@restaurant)
+      flash[:notice] = "restaurant was successfully update "
+    else
+      render :edit
+      flash[:alert] = "restaurant was failed to update"
+    end 
+  end
   private 
   def restaurant_params
     params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id]) 
   end
 end
